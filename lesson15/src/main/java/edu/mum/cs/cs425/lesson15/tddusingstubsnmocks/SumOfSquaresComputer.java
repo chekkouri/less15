936eeadf9185343d4d8e4ead8aff4185496c978f
CHEKKOURI Fatima ezzahraa <61733267+chekkouri@users.noreply.github.com>
2022-04-18 07:31:05 -0500

    Add files via upload

---
+++ lesson15/src/main/java/edu/mum/cs/cs425/lesson15/tddusingstubsnmocks/SumOfSquaresComputer.java
@@ -0,0 +1,28 @@
+package edu.mum.cs.cs425.lesson15.tddusingstubsnmocks;
+
+import java.util.Arrays;
+
+
+import edu.mum.cs.cs425.lesson15.tddusingstubsnmocks.service.ISquaresComputingService;
+
+public class SumOfSquaresComputer {
+	
+	private ISquaresComputingService sqCompService;
+
+
+	public SumOfSquaresComputer(ISquaresComputingService sqCompService) {
+		this.sqCompService = sqCompService;
+	}
+	
+	public int computeSumOfSquares(int[] ints) {
+		//int[] sqrs = sqCompService.computesSquares(ints);
+		int[] sqrs = {1,4,9};
+//		int sum = 0;
+//		for (int i : sqrs) {
+//			sum += i;
+//		}
+//		return sum;
+		return Arrays.stream(sqrs).sum();
+	}
+
+}
